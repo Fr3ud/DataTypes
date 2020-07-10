6cf39175e5d3284a1769eed22f1824b3a5056b2e
Fr3ud <oo.pavelko@gmail.com>
2020-07-11 00:02:15 +0300

    Add exercise 4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const countTypesInArray = null;
+const arr = [true, 'hello', 5, 12, -200, false, false, 'word'];
+
+const countTypesInArray = arr => {
+  const counter = {};
+
+  for (const element of arr) {
+      const type = typeof element;
+      counter[type] ? counter[type]++ : counter[type] = 1;
+  }
+  return counter;
+};
 
 module.exports = { countTypesInArray };
